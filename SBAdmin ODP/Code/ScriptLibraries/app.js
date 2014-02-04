2965b94514bc144d0c8bb7246b3ea8afff3f966f
zeromancer1972 <obusse@googlemail.com>
2014-02-04 22:40:23 +0100

    Table sort after partial refresh of view control

--- SBAdmin ODP/Code/ScriptLibraries/app.js
+++ SBAdmin ODP/Code/ScriptLibraries/app.js
@@ -1,1 +1,22 @@
 // core app library
+dojo._xhr = dojo.xhr;
+var loadOld;
+function hijacked(response, ioArgs) {
+	// your code or function here (pre-processed)
+	loadOld(response, ioArgs);
+	// your code or function here (post-processed)
+	$("table").tablesorter( {
+		debug : false
+	});
+}
+dojo.xhr = function(mode, args, bool) {
+	loadOld = args["load"];
+	args["load"] = hijacked;
+	dojo._xhr(mode, args, bool);
+}
+
+dojo.addOnLoad( function() {
+	$("table").tablesorter( {
+		debug : false
+	});
+})
